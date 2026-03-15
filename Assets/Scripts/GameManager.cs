37b58532ebc41f851095ac9a5e666c3d00a00afa
EMP-G <thor6018@gmail.com>
2026-03-15 15:39:14 +0900

    Add Q-learning FlappyBird

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,29 +2,36 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+
 public class GameManager : MonoBehaviour
 {
-    public int score = 0;
-    private int bestScore = 0;
     public TextMeshProUGUI playInfoText;
     public Button restartButton;
     
+    private int score = 0;
+    private static int bestScore = 0;
+
     void Start()
     {
         restartButton.onClick.AddListener(Restart);
         restartButton.gameObject.SetActive(false);
-
     }
-    public void AddScore(int value =1)
-    {
-        score += value;
-        if(score>bestScore) bestScore = score;
-        Debug.Log(score);
-    }
+    
     void Update()
     {
-        if (playInfoText != null) playInfoText.text = $"Score: {score}\n" + $"Best:{bestScore}";
+        playInfoText.text = $"Score: {score}\n"+
+                            $"Best: {bestScore}";
     }
+    
+    public void AddScore(int value = 1)
+    {
+        score += value;
+
+        if (score > bestScore)
+            bestScore = score;
+        Debug.Log(score);
+    }
+
     public void GameOver()
     {
         restartButton.gameObject.SetActive(true);
@@ -33,7 +40,8 @@ public class GameManager : MonoBehaviour
 
     public void Restart()
     {
-        Time.timeScale = 1;
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        SceneManager.LoadScene(
+            SceneManager.GetActiveScene().name);
+        Time.timeScale = 1f;
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,12 +1,11 @@
 using System;
 using UnityEngine;
-using UnityEngine.UI;
+
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody _rigidbody;
     public float jumpForce = 5f;
     private GameManager _gameManager;
-
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,27 +14,6 @@ public class PlayerController : MonoBehaviour
         _gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
-    
-    void OnCollisionEnter(Collision collision){
-        Debug.Log("CollisionEnter");
-        _gameManager.GameOver();
-    }
-
-    /*private void OnTriggerEnter(Collider other)
-    {Debug.Log("OnTriggerEnter");}
-    private void OnTriggerStay(Collider other)
-    {Debug.Log("OnTriggerStay");}*/
-
-    private void OnTriggerExit(Collider other)
-    {
-        Debug.Log("TO do :점수 증가");
-        _gameManager.AddScore();
-        
-    }
-
-
-                
-
     // Update is called once per frame
     void Update()
     {
@@ -43,9 +21,31 @@ public class PlayerController : MonoBehaviour
 
     void OnJump()
     {
-        // 위로 이동에 대한 action
-        Debug.Log("Jump");
+        // 위로 이동에 대한 1action
+        // Debug.Log("Jump");
         _rigidbody.linearVelocity = Vector3.up * jumpForce;
         // _rigidbody.AddForce(Vector3.up * 5f, ForceMode.VelocityChange);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        // Debug.Log("CollisionEnter");
+        _gameManager.GameOver();
+    }
+    //
+    // void OnCollisionStay(Collision collision)
+    // {
+    //     Debug.Log("OnCollisionStay");
+    // }
+    //
+    // private void OnCollisionExit(Collision other)
+    // {
+    //     Debug.Log("OnCollisionExit");
+    // }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("TODO: 점수 증가");
+        _gameManager.AddScore();
     }
 }
